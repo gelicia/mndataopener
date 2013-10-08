0ac14a571dcbaca3ce8880964e92609efa74ab67
gelicia <kdurivage@gelicia.com>
2013-10-07 22:20:36 -0500

    go home minnesota you're drunk

--- js/main.js
+++ js/main.js
@@ -6,9 +6,19 @@ var svg = d3.selectAll("svg#mapMain")
     .attr("width", width)
     .attr("height", height);
 
+var projection = d3.geo.albers()
+    .center([-94, 46])
+    .rotate([0, 0])
+    .parallels([50, 60])
+    .scale(8000)
+    .translate([width / 2, height / 2]);
+
+var path = d3.geo.path()
+    .projection(projection);
+
 	d3.json("data/mncounties.json", function(error, mn) {
 		svg.append("path")
 	    .datum(topojson.feature(mn, mn.objects.counties))
-	    .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+	    .attr("d", path);
 	});
 }
